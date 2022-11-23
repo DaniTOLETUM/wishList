bdf71b15f821cfb0927ef7aa99620b58c7e7348e
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2022-11-23 20:21:54 +0100

    Add proposito and increment counter

--- app.js
+++ app.js
@@ -5,20 +5,31 @@ let input = document.getElementById("input");
 let btn = document.getElementById("btn-create");
 let propositos = document.getElementById("lista-propositos");
 let cumplidos = document.getElementById("lista-cumplidos");
-
-
-console.log(input);
-console.log(btn);
-console.log(propositos);
-console.log(cumplidos);
+let cantidadPendientes = document.getElementById("pendientes");
+let cantidadCumplidos = document.getElementById("cumplidos");
+let propsCounter;
 
 
 // creating a proposito
 btn.onclick = createPropo;
 
 function createPropo(e) {
-    console.log("button clicked");
+    console.log(e);
+    //tqke the vqlue from input
     let entrada = input.value;
     console.log(entrada);
+
+    //create an element for the list
+    let li = document.createElement("li");
+    li.textContent = entrada;
+
+    //add the element to the list 
+    propositos.appendChild(li);
+
+    //increment the number of propositos
+    propsCounter = cantidadPendientes.innerHTML;
+    propsCounter ++;
+    cantidadPendientes.innerHTML = propsCounter
+
 }
 
