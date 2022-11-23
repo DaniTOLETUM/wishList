cb4c4f4b3238f01cb98f134d8e8264f6a15dd1c4
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2022-11-23 10:02:49 +0100

    JS file created and elements defined from document

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("starting app");
+
+//Setting variables
+let input = document.getElementById("input");
+let btn = document.getElementById("btn-create");
+let propositos = document.getElementById("lista-propositos");
+let cumplidos = document.getElementById("lista-cumplidos");
+
+
+console.log(input);
+console.log(btn);
+console.log(propositos);
+console.log(cumplidos);
+
+
